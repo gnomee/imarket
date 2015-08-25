e62b578735ef4f9e82e327beae19b635d5ac00bd
Bahoriddin Dushabaev <bahoriddin@gmail.com>
2015-08-25 14:40:24 +0600

    test commit

--- app/src/main/java/info/androidhive/loginandregistration/MainActivity.java
+++ app/src/main/java/info/androidhive/loginandregistration/MainActivity.java
@@ -11,7 +11,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-//echoechoechoehcohec
+
 public class MainActivity extends Activity {
 
 	private TextView txtName;
